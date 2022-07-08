304425bf3a052e1bb432447cf48b59204b968e14
vadim10 <vadimn1917@gmail.com>
2022-07-08 12:06:58 +0600

    Examples008_DogAnd2Friends

---
+++ Examples008_DogAnd2Friends/Program.cs
@@ -0,0 +1,13 @@
+﻿int speedFirstFriend = 2;
+int speedSecondFriend = 1;
+int speedDog = 5;
+int distanse = 110;
+int count = 0;
+while( distanse > 0){
+    distanse = distanse - speedFirstFriend - speedDog;
+    while( distanse > 0 ){
+        distanse = distanse - speedSecondFriend;
+        count++;
+    }
+}
+Console.WriteLine(count);
