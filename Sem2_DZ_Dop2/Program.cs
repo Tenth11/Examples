26f493ba00b07cb951f2ab124256074585f316e9
vadim10 <vadimn1917@gmail.com>
2022-07-13 15:06:55 +0600

    Sem2_DZ_Dop2

---
+++ Sem2_DZ_Dop2/Program.cs
@@ -0,0 +1,10 @@
+﻿double x = 10;
+int y = 20;
+int day = 1;
+while(y >= x)
+{
+  x = x *1.15;
+  Console.WriteLine((int)x);
+  day++;
+}
+Console.WriteLine("Через: " + day + " дней пробег спортсмена составит не менее " + y + " километров");
