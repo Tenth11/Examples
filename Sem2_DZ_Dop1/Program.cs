b675c945e02bb2da460904fac8cda6f1b2e3b586
vadim10 <vadimn1917@gmail.com>
2022-07-13 14:41:54 +0600

    Sem2_DZ_Dop1

---
+++ Sem2_DZ_Dop1/Program.cs
@@ -0,0 +1,10 @@
+﻿double x = 100;
+int y = 200;
+int year = 0;
+while(y >= x)
+{
+  x = x *1.1;
+  Console.WriteLine((int)x);
+  year++;
+}
+Console.WriteLine("Через: " + year + " вклад составит не менее " + y + " рублей");
