87231947d50093e86428d7fe996be7a0fb5130a7
vadim10 <vadimn1917@gmail.com>
2022-07-13 12:45:27 +0600

    Sem2_DZ_13

---
+++ Sem2_DZ_13/Program.cs
@@ -0,0 +1,6 @@
+﻿int x = new Random().Next(1, 100000);;
+Console.WriteLine(x);
+if(x < 100)
+  Console.WriteLine("Третьей цифры нет");
+ else 
+  Console.Write("Третья цифра: " + Convert.ToString(x)[2]);
