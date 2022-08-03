88b5562a7f6b7d68556338774ba2e6f7fbc85888
vadim10 <vadimn1917@gmail.com>
2022-08-03 12:21:18 +0600

    Sem4_DZ_27

---
+++ Sem4_DZ_27/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int a = Convert.ToInt32(Console.ReadLine());
+int count =0;
+int num = 0;
+int res = 0;
+while(a !=0){
+    num = a % 10;
+    a = a / 10;
+    res = res + num;
+    count++;
+}
+Console.WriteLine(res);
