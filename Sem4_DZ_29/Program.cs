ef9208adc70b3927e1aaf109778cbb6e6975b5b1
vadim10 <vadimn1917@gmail.com>
2022-08-03 12:31:01 +0600

    Sem4_DZ_29

---
+++ Sem4_DZ_29/Program.cs
@@ -0,0 +1,6 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] array = new int[8];
+for (int i = 0; i < 8; i++)
+array[i] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("[" + string.Join(", ", array) + "]");
