d259c8f32fbeda772adf0c78d6f7560648c0779a
vadim10 <vadimn1917@gmail.com>
2022-08-06 12:26:15 +0600

    Sem4_DZ36

---
+++ Sem5_DZ36/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+int n = 10;
+int[] array = new int[n];
+int summ = 0;
+for (int i = 0; i < n; i++)
+{
+array[i] = new Random().Next(0, 10);
+if (i % 2 != 0)
+summ = summ + array[i];
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine(summ);
