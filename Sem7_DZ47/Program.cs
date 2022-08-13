54d81b308ab1552e6a6f922e9b884d949cc50568
vadim10 <vadimn1917@gmail.com>
2022-08-13 09:55:57 +0600

    Sem7_DZ47

---
+++ Sem7_DZ47/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[n, m];
+for (int i = 0; i < n; i++)
+{
+for (int j = 0; j < m; j++)
+matrix[i, j] = new Random().NextDouble() * new Random().Next(-10,10);
+}
+for (int i = 0; i < n; i++)
+{
+for (int j = 0; j < m; j++)
+Console.Write(Math.Round(matrix[i, j], 1) + " \t");
+Console.WriteLine();
+}
